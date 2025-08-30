7d07a9eb0036b36af83743428898b9b1c3ae9663
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2025-08-30 14:45:45 +0500

    edit api.js

--- frontend/src/app/pages/admin/libs/api.js
+++ frontend/src/app/pages/admin/libs/api.js
@@ -1,6 +1,11 @@
 // frontend/src/lib/api.js (example)
 import axios from 'axios';
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:3000" // your local backend
+    : "https://private-blue-kappa.vercel.app" // deployed backend
 });
 export default api;
+
+
+ 
